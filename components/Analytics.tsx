6fb3b3103763b4be1b34a0e708a0f165384e45a0
Josué Silva Galvão <info@studioarthub.com>
2025-11-08 03:12:20 -0300

    Update Analytics.tsx

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,28 +1,32 @@
-import Head from "next/head";
-import React from "react";
+// components/Analytics.tsx
 import Script from "next/script";
+import React from "react";
 
 const isProd = process.env.NODE_ENV === "production";
 
-export default function Analytics(): React.ReactElement | null {
+/**
+ * Carrega Google Tag Manager somente em produção.
+ * Importante: <Script /> deve ficar FORA de <Head>.
+ * Dica: o <noscript> recomendado pelo GTM deve ser adicionado em _document.tsx (body).
+ */
+export default function Analytics(): JSX.Element | null {
   if (!isProd) return null;
 
   return (
-    <Head>
-    </Head>
-          {/* Google Tag Manager (substitua GTM-XXXXXXX pelo seu ID real quando tiver) */}
-      <Script
-        id="ga"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');
-          `,
-        }}
-      />
+    <>
+      {/* Inicializa dataLayer cedo */}
+      <Script id="gtm-datalayer" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];`}
+      </Script>
+
+      {/* Google Tag Manager (substitua GTM-XXXXXXX pelo ID real) */}
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-XXXXXXX');`}
+      </Script>
+    </>
   );
 }

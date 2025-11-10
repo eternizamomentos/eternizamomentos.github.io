082c4dc4a1fa3d4ba8b60af1824d6f1d8728d339
Josué Silva Galvão <info@studioarthub.com>
2025-11-09 21:12:09 -0300

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Garante que o build seja estático
-  basePath: '', // deixe vazio se estiver usando domínio próprio no CNAME
-  images: {
-    unoptimized: true, // evita erro de otimização no GitHub Pages
-  },
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '', // nada aqui!
+  assetPrefix: '.', // força caminhos relativos
+  trailingSlash: true, // garante compatibilidade com export estático
 };
 
 export default nextConfig;
